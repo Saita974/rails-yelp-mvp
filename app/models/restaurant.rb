2ce7b55de5624ebc1679e061534472f0def59e5e
matthias sansig <m.sansig@gmail.com>
2022-08-18 11:21:19 +0200

    restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
   validates :name, presence: true
   validates :address, presence: true
-  VALID_CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  VALID_CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian", "indian"]
   validates :category, presence: true, inclusion: { in: VALID_CATEGORIES }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants, only: [:edit, :index, :update, :show] do
+    resources :restaurants, only: [:edit, :index, :update, :show]
+  end
 end
